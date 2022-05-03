08785be00daa1a0763f4bd0de8c2113fd163e241
Carly-Freedman <freedmcb0023@student.laccd.edu>
2022-05-03 14:07:00 -0700

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,9 @@
 
 public class Demo{
     public static String division(int n1, int n2){
+        if (n2 == 0){
+            return "undefined";
+        }
         return String.valueOf(n1/n2);
     }
 }
